7a94ee8ebe437dbc2baedd3ba44a12f6d6720de5
arindam-sarkar <arisarkar.mail@gmail.com>
2022-08-21 11:02:12 +0530

    loader for img added

--- src/Components/ImageCard/ImageCard.js
+++ src/Components/ImageCard/ImageCard.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ImageCard.css";
-import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const ImageCard = ({ name, url }) => {
+  const [loading, setLoading] = useState(true);
+
+  const imageLoaded = () => {
+    setLoading(false);
+  };
   return (
-    <div className="image_container">
-      <p>This is Carousel {name}</p>
-      <LazyLoadImage alt={name} src={`${url}.jpg`} />
-    </div>
+    <>
+      <div id="loader" style={{ display: loading ? "block" : "none" }}>
+        <div className="spinner">
+          <div className="nb-spinner"></div>
+        </div>
+      </div>
+
+      <div style={{ display: loading ? "none" : "block" }}>
+        <div className="image_container">
+          <p>This is Carousel {name}</p>
+          <img onLoad={imageLoaded} alt={name} src={`${url}.jpg`} />
+        </div>
+      </div>
+    </>
   );
 };
 
